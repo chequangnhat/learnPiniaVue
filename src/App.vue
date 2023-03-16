03a98f5ef91a1fef2c72fe8be16d29cdef649613
nhatche42 <nhatche42@gmail.com>
2023-03-16 16:18:46 +0700

    hh

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useUserStore } from "./stores/users";
+import { ref } from "vue";
+const user_store = useUserStore();
+
+const user_input = ref({
+  name: "",
+  email: "",
+});
+
+const sort = ref(false);
+
+const CreateUser = () => {
+  user_store.create(user_input.value);
+  user_input.value = {
+    name: "",
+    email: "",
+  };
+};
+</script>
+
+<template>
+  <form action="" @submit.prevent="CreateUser">
+    <input type="text" v-model="user_input.name">
+    <input type="text" v-model="user_input.email">
+    <input type="submit" value="submit">
+  </form>
+  <div v-for="user in user_store.users">{{ user }}</div>
+</template>
+
+<style scoped></style>
